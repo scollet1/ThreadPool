d33f52058d1f4630192a78218417a340db79f9c4
Samuel Collet <samuelpcollet@gmail.com>
2020-09-24 12:11:31 -0700

    updated to my writing style, implemented singleton, added header guard

--- ThreadPool.h
+++ ThreadPool.h
@@ -1,57 +1,38 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
 #pragma once
 
+#include <mutex>
+#include <queue>
+#include <atomic>
+#include <future>
 #include <thread>
 #include <vector>
-#include <queue>
-#include <mutex>
-#include <condition_variable>
-#include <functional>
-#include <future>
-#include <atomic>
-#include <type_traits>
 #include <typeinfo>
+#include <functional>
+#include <type_traits>
+#include <condition_variable>
 
 /* ThreadPool class */
-class ThreadPool
-{
+class ThreadPool {
+
 public:
-
-	ThreadPool()
-	{
-		m_shutdown.store(false, std::memory_order_relaxed);
-		//m_shutdown = false;
-		createThreads(1);
-	}
-
-	ThreadPool(std::size_t numThreads)
-	{
-		m_shutdown.store(false, std::memory_order_relaxed);
-		//m_shutdown = false;
-		createThreads(numThreads);
-	}
-
-	~ThreadPool()
-	{
-		m_shutdown.store(true, std::memory_order_relaxed);
-		//m_shutdown = true;
-		m_notifier.notify_all();
-
-		for (std::thread& th : m_threads)
-		{
-			th.join();
-		}
+	static ThreadPool* get_instance() {	
+		if (!instance)
+			instance = new ThreadPool;
+		return instance;
 	}
 
 	//add any arg # function to queue
 	template <typename Func, typename... Args>
-	auto enqueue(Func&& f, Args&&... args)
-	{
+	auto submit(Func&& f, Args&&... args) {
 		//get return type of the function
 		using RetType = std::invoke_result_t<Func, Args...>;
 
-		auto task = std::make_shared<std::packaged_task<RetType()>>([&f, &args...]() { return f(std::forward<Args>(args)...); });
-
-		{
+		auto task = std::make_shared<std::packaged_task<RetType()>>([&f, &args...]() {
+			return f(std::forward<Args>(args)...);
+		}); {
 			// lock jobQueue mutex, add job to the job queue 
 			std::scoped_lock<std::mutex> lock(m_jobMutex);
 
@@ -71,33 +52,46 @@ public:
 	}
 
 private:
+	static ThreadPool* instance;
 
-	using Job = std::function<void()>;
-	std::vector<std::thread> m_threads;
-	std::queue<Job> m_jobQueue;
-	std::condition_variable m_notifier;
-	std::mutex m_jobMutex;
-	std::atomic<bool> m_shutdown;
+	ThreadPool() {
+		m_shutdown.store(false, std::memory_order_relaxed);
+		//m_shutdown = false;
+		createThreads(1);
+	}
 
-	void createThreads(std::size_t numThreads)
-	{
+	ThreadPool(std::size_t numThreads) {
+		m_shutdown.store(false, std::memory_order_relaxed);
+		//m_shutdown = false;
+		createThreads(numThreads);
+	}
+
+	~ThreadPool() {
+		m_shutdown.store(true, std::memory_order_relaxed);
+		//m_shutdown = true;
+		m_notifier.notify_all();
+
+		for (std::thread& th : m_threads) {
+			th.join();
+		}
+	}
+
+	void createThreads(std::size_t numThreads) {
 		m_threads.reserve(numThreads);
-		for (int i = 0; i != numThreads; ++i)
-		{
-			m_threads.emplace_back(std::thread([this]()
-			{
-				while (true)
-				{
+		for (int i = 0; i != numThreads; ++i) {
+			m_threads.emplace_back(std::thread([this]() {
+				while (true) {
 					Job job;
 
 					{
 						std::unique_lock<std::mutex> lock(m_jobMutex);
-						m_notifier.wait(lock, [this] {return !m_jobQueue.empty() || m_shutdown.load(std::memory_order_relaxed); });
+						m_notifier.wait(lock, [this] {
+							return (
+								!m_jobQueue.empty() || m_shutdown.load(std::memory_order_relaxed)
+							);
+						});
 
-						if (m_shutdown.load(std::memory_order_relaxed))
-						{
-							break;
-						}
+						if (m_shutdown.load(std::memory_order_relaxed)) break;
 
 						job = std::move(m_jobQueue.front());
 
@@ -108,4 +102,17 @@ private:
 			}));
 		}
 	}
+
+	using Job = std::function<void()>;
+	std::vector<std::thread> m_threads;
+	std::queue<Job> m_jobQueue;
+	std::condition_variable m_notifier;
+	std::mutex m_jobMutex;
+	std::atomic<bool> m_shutdown;
+
 }; /* end ThreadPool Class */
+
+ThreadPool* ThreadPool::instance = 0;
+ThreadPool* ThreadPool = ThreadPool->get_instance();
+
+#endif
